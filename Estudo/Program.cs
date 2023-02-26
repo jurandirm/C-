76c5568a5146e2c42310a65100e5d54746901623
Jurandir Marinho <jurandirmarinho13@gmail.com>
2023-02-26 16:10:34 -0300

    first commit

---
+++ Estudo/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        Console.WriteLine("*** ESTUDOS ***");
+        Console.WriteLine(" ");
+
+        Console.Write("Digite seu nome: ");
+        string? nome = Console.ReadLine();
+
+        Console.WriteLine("Seja bem vindo " + nome);
+    }
+}
